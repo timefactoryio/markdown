821bf28de72d19687465688d7811397e7c26f88c
zachklingbeil <zach@zachklingbeil.com>
2026-05-25 13:43:46 -0700

    added styles

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package markdown
 import (
 	"io"
 
+	"github.com/alecthomas/chroma/v2"
 	"github.com/yuin/goldmark"
 	highlighting "github.com/yuin/goldmark-highlighting/v2"
 	"github.com/yuin/goldmark/ast"
@@ -13,6 +14,47 @@ import (
 	"github.com/yuin/goldmark/util"
 	"go.abhg.dev/goldmark/toc"
 )
+
+type Style struct {
+	Background  string
+	Foreground  string
+	Error       string
+	Keyword     string
+	Operator    string
+	Punctuation string
+	Name        string
+	Literal     string
+	Comment     string
+	Generic     string
+}
+
+var Default = Style{
+	Background:  "#1d2432",
+	Foreground:  "#ffffff",
+	Error:       "#ff636f",
+	Keyword:     "#ff636f",
+	Operator:    "#ff636f",
+	Punctuation: "#ffffff",
+	Name:        "#58a1dd",
+	Literal:     "#a6be9d",
+	Comment:     "italic #828b96",
+	Generic:     "#ffffff",
+}
+
+func (s Style) style() *chroma.Style {
+	return chroma.MustNewStyle("theme", chroma.StyleEntries{
+		chroma.Background:  s.Foreground + " bg:" + s.Background,
+		chroma.Other:       s.Foreground,
+		chroma.Error:       s.Error,
+		chroma.Keyword:     s.Keyword,
+		chroma.Operator:    s.Operator,
+		chroma.Punctuation: s.Punctuation,
+		chroma.Name:        s.Name,
+		chroma.Literal:     s.Literal,
+		chroma.Comment:     s.Comment,
+		chroma.Generic:     s.Generic,
+	})
+}
 
 type imageUnwrap struct{}
 
@@ -30,11 +72,11 @@ type Markdown struct {
 	md goldmark.Markdown
 }
 
-func New() *Markdown {
+func New(style Style) *Markdown {
 	return &Markdown{md: goldmark.New(
 		goldmark.WithExtensions(
 			extension.GFM,
-			highlighting.NewHighlighting(highlighting.WithStyle("hrdark")),
+			highlighting.NewHighlighting(highlighting.WithCustomStyle(style.style())),
 		),
 		goldmark.WithParserOptions(
 			parser.WithAutoHeadingID(),
